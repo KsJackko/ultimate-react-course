7b200de7cbc6428f24289b07c13f2ef1fcfdeb39
KaSing <160307874+KsJackko@users.noreply.github.com>
2025-07-11 01:20:14 +0800

    64. React Developer Tools

--- 04-steps/starter/steps/src/App.js
+++ 04-steps/starter/steps/src/App.js
@@ -1,6 +1,65 @@
+import { useState } from 'react';
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
 export default function App() {
-  return;
-  <div>
-    <div></div>
-  </div>;
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleNextStep() {
+    setStep((step) => {
+      if (step === 3) return step;
+      return step + 1;
+    });
+  }
+
+  function handlePreviousStep() {
+    setStep((step) => {
+      if (step === 1) return step;
+      return step - 1;
+    });
+  }
+
+  function handleOpen() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div>
+      <button onClick={handleOpen} className="close">
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+            <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+            <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              onClick={handlePreviousStep}
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNextStep}
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }

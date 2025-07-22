5778d74bbe9cacc396a5956ddf99bb7b47076bb5
KaSing <160307874+KsJackko@users.noreply.github.com>
2025-07-23 01:26:27 +0800

    121. CHALLENGE #1: Text Expander Componen

--- 07-usepopcorn/starter/usepopcorn/src/index.js
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, SetMovieRating] = useState(0);
+  function handleMovieRating(rating) {
+    SetMovieRating(rating);
+  }
+  return (
+    <div>
+      <StarRating maxRating={10} onSetRating={handleMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={'sss'}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating
+      maxRating={5}
+      color={'red'}
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );

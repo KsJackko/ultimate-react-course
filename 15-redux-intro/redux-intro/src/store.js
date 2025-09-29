7526fdf2719ad6fc0168cb6e5d236c548fa24eaa
ZKS456 <jackko.zhang@ha.org.hk>
2025-09-29 17:30:17 +0800

    265. Adding More State: Customer

--- 15-redux-intro/redux-intro/src/store.js
+++ 15-redux-intro/redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -11,11 +13,63 @@ function reducer(state = initialState, action) {
     case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
     case 'account/requestLoan':
-      if (state.loan > 0) {
-        return { ...state };
-      }
-      return {...state, loan: state.loan + 5000, state.balance+5000}；
+      if (state.loan > 0) return state;
+      // Later
+      return {
+        ...state,
+        balance: state.balance + action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        loan: state.loan + action.payload.amount,
+      };
+    case 'account/payLoan':
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: '',
+        balance: state.balance - state.loan,
+      };
     default:
       return state;
   }
 }
+
+const store = createStore(reducer);
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// console.log(store.getState());
+// store.dispatch({ type: 'account/withdraw', payload: 200 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: 'account/requestLoan',
+//   payload: { amount: 1000, purpose: 'Buy a car' },
+// });
+// console.log(store.getState());
+// store.dispatch({ type: 'account/payLoan' });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose: 'Buy a car' },
+  };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+console.log(store.getState());
+store.dispatch(withdraw(200));
+console.log(store.getState());
+store.dispatch(requestLoan(1000));
+console.log(store.getState());
+store.dispatch(payLoan());
+console.log(store.getState());
